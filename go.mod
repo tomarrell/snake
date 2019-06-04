2f12e1e89e8e0641667213f3fdcd52166e914a92
Tom Arrell <tom.arrell@gmail.com>
2019-06-04 13:03:53 +0200

    Actually added token endpoint...

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/tomarrell/snake
 
 go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.2
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5 // indirect
+	golang.org/x/net v0.0.0-20190603091049-60506f45cf65 // indirect
+	golang.org/x/sys v0.0.0-20190602015325-4c4f7f33c9ed // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20190603231351-8aaa1484dc10 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	uuid "github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	port = ":8080"
+)
+
+/// Serve a token to the client to check future requests
+func generateToken(w http.ResponseWriter, r *http.Request) {
+	u1 := uuid.NewV4()
+	respondJSON(w, token{UUID: u1.String()})
+}
 
 func main() {
-	fmt.Println("Hello World")
+	db, err := sql.Open("sqlite3", "./db.db")
+	if err != nil {
+		log.Fatal("Failed to open sqlite DB")
+	}
+	defer db.Close()
+
+	r := mux.NewRouter()
+
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/token", generateToken)
+
+	log.Info("Starting server of port ", port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
